517faca5bb0ea8bdd184971d3e722cec608e31a5
Dante Rodríguez Chambi <dantesamuelrodriguez@gmail.com>
2024-07-27 20:42:46 -0500

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
  import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://back-task-3d72.onrender.com/api',
   withCredentials: true
 })
 
